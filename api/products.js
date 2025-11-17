5ae47d7bd0bd86b8aa92349b72a2b5f94cddf22a
clarasophiacullen95-cloud <clarasophiacullen95@gmail.com>
2025-11-17 13:52:27 +0000

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,11 +1,12 @@
-import { connectDB } from '../lib/db';
+import { connectDB } from "../lib/db.js";
 
 export default async function handler(req, res) {
   try {
     const db = await connectDB();
-    const products = await db.collection('products').find().toArray();
+    const products = await db.collection("products").find({}).toArray();
     res.status(200).json(products);
   } catch (err) {
+    console.error("Products error:", err);
     res.status(500).json({ error: err.message });
   }
 }
